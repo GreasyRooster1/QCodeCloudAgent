4a4243a0c23e302fe15e134370abcd801d1606dd
GreasyRooster <greasyRooster1@gmail.com>
2025-01-24 14:40:38 -0800

    install script

---
+++ install/install.bat
@@ -0,0 +1,6 @@
+set key=qcodecloudagent
+reg add HKCR\%key% /ve /d "URL:Description"
+reg add HKCR\%key% /v "URL Protocol" /d ""
+reg add HKCR\%key%\shell
+reg add HKCR\%key%\shell\open
+reg add HKCR\%key%\shell\open\command /ve /d "\"C:\Program Files\QCodeCloudAgent\protocol.exe\" \"%%1\""
