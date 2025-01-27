5ec718c13a6d5b4c667c89b18089b79dabf13f67
GreasyRooster <greasyRooster1@gmail.com>
2025-01-27 11:37:08 -0800

    run cli command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use std::process::Command;
 use std::string::ToString;
 use rouille::router;
 use serde::Serialize;
 
 const PORT:i32 = 8181;
-const SKETCH_FOLDER:&str = "./sketches";
+const SKETCHES_FOLDER:&str = "./sketches";
 
 #[derive(Serialize)]
 struct CreateResponse{
@@ -16,10 +17,16 @@ fn main() {
     rouille::start_server(format!("localhost:{PORT}"), move |request| {
         router!(request,
 
-            (GET) (/create/{id:String}) => {
+            (GET) (/create/{name:String}) => {
                 let mut response = &CreateResponse{
                     ok:true,
                 };
+
+                run_cli_command(vec![
+                    "sketch".to_string(),
+                    "new".to_string(),
+                    name
+                ]);
 
                 rouille::Response::json(response)
             },
@@ -31,4 +38,12 @@ fn main() {
             _ => rouille::Response::empty_404()
         )
     });
+}
+
+fn run_cli_command(args:Vec<String>){
+    Command::new("arduino-cli")
+        .current_dir(SKETCHES_FOLDER)
+        .args(args)
+        .spawn()
+        .expect("command failed");
 }
