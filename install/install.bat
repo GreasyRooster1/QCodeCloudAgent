b03d70f0b34c9d802faf1b58e459161fc3e374c0
GreasyRooster <greasyRooster1@gmail.com>
2025-02-01 15:43:53 -0800

    install

--- install/install.bat
+++ install/install.bat
@@ -1,6 +1,76 @@
+@echo off
+::::::::::::::::::::::::::::::::::::::::::::
+:: Elevate.cmd - Version 8
+:: Automatically check & get admin rights
+:: see "https://stackoverflow.com/a/12264592/1016343" for description
+::::::::::::::::::::::::::::::::::::::::::::
+
+ CLS
+ ECHO.
+ ECHO =============================
+ ECHO Running Admin shell
+ ECHO =============================
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~dpnx0"
+ rem this works also from cmd shell, other than %~0
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  whoami /groups /nh | find "S-1-16-12288" > nul
+  if '%errorlevel%' == '0' ( goto checkPrivileges2 ) else ( goto getPrivileges )
+
+
+:checkPrivileges2
+  net session 1>nul 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+ ::::::::::::::::::::::::::::
+ ::START
+ ::::::::::::::::::::::::::::
+
 set key=qcodecloudagent
-reg add HKCR\%key% /ve /d "QCode Cloud Agent"
-reg add HKCR\%key% /v "URL Protocol" /d ""
-reg add HKCR\%key%\shell
-reg add HKCR\%key%\shell\open
-reg add HKCR\%key%\shell\open\command /ve /d "\"C:\Program Files\QCodeCloudAgent\protocol.exe\" \"%%1\""
+reg add HKCR\%key% /ve /d "QCode Cloud Agent" /f
+reg add HKCR\%key% /v "URL Protocol" /d "" /f
+reg add HKCR\%key%\shell /f
+reg add HKCR\%key%\shell\open /f
+reg add HKCR\%key%\shell\open\command /f /ve /d "\"C:\Program Files\QCodeCloudAgent\QCodeCloudAgent.exe\" \"%%1\""
+
+mkdir "C:\Program Files\QCodeCloudAgent\"
+mkdir "C:\Program Files\QCodeCloudAgent\sketches"
+copy .\QCodeCloudAgent.exe "C:\Program Files\QCodeCloudAgent\sketches\QCodeCloudAgent.exe "
